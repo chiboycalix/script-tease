6be901e2f5c903d7fe58ecbdcc910eae60b3a613
Igwe Chinonso <igwechinonso77@gmail.com>
2024-05-10 08:11:08 +0100

    more responsiveness

--- app/sections/home/index.js
+++ app/sections/home/index.js
@@ -2,8 +2,8 @@
 import Button from "@/components/button";
 import React from "react";
 import { motion } from "framer-motion";
-import "./home.css";
 import ArrowIcon from "../../../assets/arrow.png";
+import "./home.css";
 
 const FirstSection = () => {
   return (
@@ -41,49 +41,10 @@ const FirstSection = () => {
         </div>
       </motion.div>
       <div className="flex-1 relative">
-        <motion.div
-          className="absolute bg-gradient-to-r rounded-full from-green-500 to-slate-50
-          
-                     sm:w-[14rem] sm:h-[14rem] sm:top-[10rem] sm:left-[15rem]
-                     md:w-[12rem] md:h-[12rem] md:top-[15rem] md:left-[26rem]
-                     lg:w-[12rem] lg:h-[12rem] lg:top-[14rem] lg:left-[7rem]
-                     xl:w-[17rem] xl:h-[17rem] xl:top-[12rem] 
-                     2xl:w-[20rem] 2xl:h-[20rem] 2xl:top-[12rem] 2xl:left-[15rem]
-                     "
-          initial={{ opacity: 0, x: 150 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.7 }}
-        ></motion.div>
-        <div
-          className="second absolute -z-50 bg-gradient-to-t from-blue-900 to-slate-50 -rotate-[25deg] rounded-3xl
-          sm:w-[13rem] sm:h-[24rem]
-          md:w-[11rem] md:h-[24rem]          
-          lg:w-[11rem] lg:h-[24rem]           
-                    xl:w-[12rem] xl:h-[24rem]
-                    2xl:w-[15rem] 2xl:h-[24rem]"
-        ></div>
-
-        <motion.div
-          className="absolute bg-gradient-to-tr from-orange-500 to-slate-50 rounded-full
-          sm:w-[14rem] sm:h-[14rem] sm:top-[24rem] sm:-right-[13rem]
-          md:w-[11rem] md:h-[11rem] md:top-[24rem] md:-right-[5rem]
-                    lg:w-[11rem] lg:h-[11rem] lg:top-[20rem] lg:right-0
-                    xl:w-[16rem] xl:h-[16rem]
-                    2xl:w-[19rem] 2xl:h-[19rem]"
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          transition={{ duration: 0.7, delay: 0.7 }}
-        ></motion.div>
-
-        <div
-          className="fourth absolute bg-gradient-to-b from-green-600 to-slate-50 rounded-3xl -rotate-[25deg]
-          sm:w-[13rem] sm:h-[24rem]
-          md:w-[11rem] md:h-[24rem]                
-          lg:w-[11rem] lg:h-[24rem]  
-                    xl:w-[12rem] xl:h-[24rem]
-                    2xl:h-[24rem] 2xl:w-[15rem]
-          "
-        ></div>
+        <div className="first"></div>
+        <div className="second"></div>
+        <div className="third"></div>
+        <div className="fourth"></div>
       </div>
     </div>
   );
